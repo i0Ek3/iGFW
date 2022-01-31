3287bc62e46e2f16803dcead3f4714dd7e5377d9
i0Ek3 <kno30826@gmail.com>
2022-01-31 16:29:29 +0800

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 setup() {
-    sudo apt install -y vim zsh tree wget curl ufw screen neofetch unzip
+    sudo apt install -y git vim zsh tree wget curl ufw snapd screen neofetch unzip
     ./install_omz.sh
-    cp vimrc ~/.vimrc ; cp zshrc ~/.zshrc
     zsh
+    cp vimrc ~/.vimrc ; cp zshrc ~/.zshrc
     source ~/.vimrc ; source ~/.zshrc
-    echo "-------> Finished! <-------"
+    echo "-------> zsh/vim configured! <-------"
 }
 
-setup
+main() {
+    setup
+}
+
+main
